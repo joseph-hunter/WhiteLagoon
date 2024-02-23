1f9a840729c936cc6dcc6d932f5df9db25cc64c9
Joseph Hunter <joseph@freemountaininc.com>
2024-02-23 11:40:55 -0500

    Project Setup

---
+++ WhiteLagoon.Infrastructure/Migrations/20240223163534_UpdateColumnNames.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateColumnNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Villas",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Villas",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Villas",
+                newName: "ImageURL");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Villas",
+                newName: "ID");
+        }
+    }
+}
