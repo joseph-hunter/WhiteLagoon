48c8ba2cadb0057a9d687bdc8e16766199cc7ce9
Joseph Hunter <joseph@freemountaininc.com>
2024-03-22 12:07:14 -0400

    Finished 170

--- WhiteLagoon.Web/Controllers/BookingController.cs
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -17,6 +17,13 @@ namespace WhiteLagoon.Web.Controllers
             _unitOfWork = unitOfWork;
         }
         [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize]
         public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
@@ -109,5 +116,36 @@ namespace WhiteLagoon.Web.Controllers
             }
             return View(bookingId);
         }
+
+
+
+
+        #region API Calls
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<Booking> objBookings;
+
+            if (User.IsInRole(SD.Role_Admin))
+            {
+                objBookings = _unitOfWork.Booking.GetAll(includeProperties: "User,Villa");
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objBookings = _unitOfWork.Booking
+                    .GetAll(u => u.UserId == userId, includeProperties: "User,Villa");
+            }
+            if (!string.IsNullOrEmpty(status))
+            {
+                objBookings = objBookings.Where(u => u.Status.ToLower().Equals(status.ToLower()));
+            }
+            return Json(new { data = objBookings });
+        }
+
+        #endregion
     }
 }
